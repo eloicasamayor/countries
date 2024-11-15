40d6ee77f01ed083608bed3c92accb62cd618a0b
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2024-11-15 09:12:12 +0100

    afegit fetching a countries rest api!

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 // @ts-ignore
 import { Map } from "./assets/world";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const selectedPath = useRef<SVGPathElement | null>(null);
   const [country, setCountry] = useState("");
+  type countryInfoT = {
+    capital: string;
+    languages: object;
+  };
+  const [countryInfo, setCountryInfo] = useState<countryInfoT>({
+    capital: "",
+    languages: {},
+  });
+
+  useEffect(() => {
+    if (!country) {
+      return;
+    }
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+      .then((response) => response.json())
+      // 4. Setting *dogImage* to the image url that we received from the response above
+      .then((data) => {
+        setCountryInfo(data[0]);
+      });
+  }, [country]);
 
   useEffect(() => {
     let paths = document.querySelectorAll("path");
@@ -14,7 +35,11 @@ function App() {
   }, []);
 
   function onClickPais(e: MouseEvent) {
+    if (selectedPath.current) {
+      selectedPath.current.setAttribute("style", "fill:black");
+    }
     const targ = e.target as SVGPathElement;
+    selectedPath.current = targ;
     const title = (targ.attributes.getNamedItem("title") as Attr).value;
 
     if (!title || !targ) {
@@ -28,6 +53,15 @@ function App() {
     <>
       <p>{country}</p>
       <Map />
+      <div>
+        <p>
+          <strong>Capital:</strong> {countryInfo?.capital}
+        </p>
+        <p>
+          <strong>Languages:</strong>{" "}
+          {Object.values(countryInfo?.languages).map((l) => l + ", ")}
+        </p>
+      </div>
     </>
   );
 }
