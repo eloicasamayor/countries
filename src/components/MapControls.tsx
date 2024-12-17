086330473f7c8b71d58bdd87d7241b865ea2ed38
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2024-12-17 20:32:36 +0100

    canvi llibreria per zoom

---
+++ src/components/MapControls.tsx
@@ -0,0 +1,13 @@
+import { useControls } from "react-zoom-pan-pinch";
+
+export const MapControls = () => {
+  const { zoomIn, zoomOut, resetTransform } = useControls();
+
+  return (
+    <div className="tools">
+      <button onClick={() => zoomIn()}>+</button>
+      <button onClick={() => zoomOut()}>-</button>
+      <button onClick={() => resetTransform()}>x</button>
+    </div>
+  );
+};
